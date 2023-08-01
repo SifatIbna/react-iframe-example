ad278408238a90ddf8451d382ed4705ffd1e0e0f
Shifu <sifatibna.amin9@gmail.com>
2023-08-01 13:48:13 +0600

    added iframe tag to render prothomalo webpage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
 
 function App() {
+  const [show, setShow] = useState(false);
+  const [url, setUrl] = useState('');
+
+  const handleLinkClick = (event) => {
+    event.preventDefault();
+    setUrl(event.target.href);
+    setShow(true);
+  };
+
+  const handleClose = () => setShow(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <a href="https://www.prothomalo.com/" onClick={handleLinkClick}>
+        Click me
+      </a>
+
+      <Modal show={show} onHide={handleClose} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <iframe src={url} style={{ width: '100%', height: '500px' }} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
